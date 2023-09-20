942d0e2aa156093dd95cba8838e586da31611a92
Alicia MacEwan-Gutierrez <alicia7749@gmail.com>
2023-09-20 11:41:23 -0700

    created pages

---
+++ frontend/src/pages/SingleCocktail.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const SingleCocktail = () => {
+    return (
+        <div>
+            <h2>Single Cockatil Page</h2>
+        </div>
+    )
+}
+
+export default SingleCocktail 
